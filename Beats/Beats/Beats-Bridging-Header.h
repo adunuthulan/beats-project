fbdf1a523c8be3e7cd9513b67d8cdd45b7d80345
Nirav Adunuthula <adunuthulan@gmail.com>
2020-06-10 10:48:12 -0700

    Spotify iOS13 app and Token Swap

---
+++ Beats/Beats/Beats-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Beats-Bridging-Header.h
+//  Beats
+//
+//  Created by Nirav Adunuthula on 6/9/20.
+//  Copyright © 2020 Nirav Adunuthula. All rights reserved.
+//
+
+#ifndef Beats_Bridging_Header_h
+#define Beats_Bridging_Header_h
+
+#import <SpotifyiOS/SpotifyiOS.h>
+
+#endif /* Beats_Bridging_Header_h */
